071d2ee95c4834c713eb5aca95530e57d6c04425
arfi <arma0001@stud.kea.dk>
2025-01-25 19:11:48 +0100

    first commit

---
+++ src/main/java/com/example/somalirestaurantbackend/SomaliRestaurantBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.somalirestaurantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomaliRestaurantBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomaliRestaurantBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/somalirestaurantbackend/SomaliRestaurantBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somalirestaurantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomaliRestaurantBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
